dbfe14d16431120f223640997b57629faceadfdd
root <purnesh.xyz@gmail.com>
2014-01-06 04:25:18 +0530

    Security improvement. XSRF Protection enabled.

--- application/controllers/blog.php
+++ application/controllers/blog.php
@@ -2,10 +2,16 @@
 class Blog extends CI_Controller{
 	public function index(){
 		echo "hello world";
+		$this->load->helper('url');
+		echo anchor('blog/comments', 'Click Here');
 	}
 	
 	public function comments(){
 		echo "Comments";
+		$data['title'] = "My Real Title";
+		$data['heading'] = "My Real Heading";
+
+		$this->load->view('blogview', $data);
 	}
 	
 	public function shoes($sandals, $id){
