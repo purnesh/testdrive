96beb87ec34f57f6c1114b111a72647a3bbf8b07
Purnesh Tripathi <purnesh.xyz@gmail.com>
2014-01-07 02:54:58 +0530

    minute change

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 /*
  *---------------------------------------------------------------
- * APPLICATION ENVIRONMENT
+ * APPLICATION ENVIRONMENTERA
  *---------------------------------------------------------------
  *
  * You can load different configurations depending on your
