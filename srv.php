af512047376baf7ade133d1bb96b2231f53fc236
Purnesh Tripathi <purnesh.xyz@gmail.com>
2014-01-08 23:10:32 +0530

    Bug fixed on RegExp testing file

--- srv.php
+++ srv.php
@@ -1,7 +1,11 @@
 <?php
 
-if(isset($_POST['prncode']) && $_POST['pnrcode'] == '1111111111'){
+if(isset($_POST['pnrcode']) && $_POST['pnrcode'] == '1111111111'){
 	echo '&#Purnesh Tripathi*#S3, 54*#Confirmed* $';
 }
 
 ?>
+<form method="POST" action="srv.php">
+<input type="text" name="pnrcode" id="pnrcode"/>
+<input type="submit"/>
+</form>
