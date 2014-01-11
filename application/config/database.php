b44b95293f8a9d34561234e72c30336011354b83
root <root@www.atcad.in>
2014-01-11 17:21:40 -0500

    Submission back to the core for error check

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = 'Purmesh#1';
+$db['default']['password'] = '9897132475';
 $db['default']['database'] = 'atcad_data';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
